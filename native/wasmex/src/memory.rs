6e8805f96c6dfb3268818d420889f753a6309091
Philipp Tessenow <philipp@tessenow.org>
2020-08-23 14:27:28 +0200

    formatter format unformattedly formed forms

--- native/wasmex/src/memory.rs
+++ native/wasmex/src/memory.rs
@@ -194,9 +194,7 @@ pub fn memory_from_instance(instance: &Instance) -> Result<&Memory, Error> {
             Extern::Memory(memory) => Some(memory),
             _ => None,
         })
-        .ok_or_else(|| Error::RaiseTerm(Box::new(
-            "The WebAssembly module has no exported memory.",
-        )))
+        .ok_or_else(|| Error::RaiseTerm(Box::new("The WebAssembly module has no exported memory.")))
 }
 
 pub fn read_binary<'a>(env: RustlerEnv<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> {
