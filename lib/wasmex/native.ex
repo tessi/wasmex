d3fe26e71c05ec801ba7c62bd59eabde3f5d28ed
Chris Nelson <superchrisnelson@gmail.com>
2024-11-17 13:25:12 -0500

    remove dev junk

--- lib/wasmex/native.ex
+++ lib/wasmex/native.ex
@@ -5,9 +5,23 @@ defmodule Wasmex.Native do
   version = mix_config[:version]
   github_url = mix_config[:package][:links]["GitHub"]
 
-  use Rustler,
+  use RustlerPrecompiled,
     otp_app: :wasmex,
-    mode: :debug
+    base_url: "#{github_url}/releases/download/v#{version}",
+    version: version,
+    targets: ~w(
+      aarch64-apple-darwin
+      aarch64-unknown-linux-gnu
+      aarch64-unknown-linux-musl
+      riscv64gc-unknown-linux-gnu
+      x86_64-apple-darwin
+      x86_64-pc-windows-gnu
+      x86_64-pc-windows-msvc
+      x86_64-unknown-freebsd
+      x86_64-unknown-linux-gnu
+      x86_64-unknown-linux-musl
+    ),
+    force_build: System.get_env("WASMEX_BUILD") in ["1", "true"]
 
   def engine_new(_engine_config), do: error()
   def engine_precompile_module(_engine_resource, _bytes), do: error()
