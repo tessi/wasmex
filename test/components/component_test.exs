f05e1abfb49483d116d4306401b9da6f2da92a11
Chris Nelson <superchrisnelson@gmail.com>
2024-11-26 17:53:12 -0500

    refactor tests a bit

--- test/components/component_test.exs
+++ test/components/component_test.exs
@@ -8,38 +8,13 @@ defmodule Wasmex.WasmComponentsTest do
     {:ok, store} = Wasmex.Components.Store.new(%Wasmex.Wasi.WasiP2Options{})
     component_bytes = File.read!("test/component_fixtures/hello_world/hello_world.wasm")
     {:ok, component} = Wasmex.Components.Component.new(store, component_bytes)
-    IO.inspect("building instance")
     {:ok, instance} = Wasmex.Components.Instance.new(store, component)
-    IO.inspect("executing component function")
 
     assert {:ok, "Hello, Elixir!"} =
              Wasmex.Components.Instance.call_function(instance, "greet", ["Elixir"])
 
     assert {:ok, ["Hello, Elixir!", "Hello, Elixir!"]} =
              Wasmex.Components.Instance.call_function(instance, "multi-greet", ["Elixir", 2])
-  end
-
-  test "functions with maps and lists" do
-    {:ok, store} = Wasmex.Components.Store.new(%Wasmex.Wasi.WasiP2Options{inherit_stdout: true})
-
-    component_bytes = File.read!("test/component_fixtures/live_state/live-state.wasm")
-    {:ok, component} = Wasmex.Components.Component.new(store, component_bytes)
-    {:ok, instance} = Wasmex.Components.Instance.new(store, component)
-
-    assert {:ok, %{"customers" => [customer]} = state} =
-             Wasmex.Components.Instance.call_function(instance, "init", [])
-
-    assert customer["email"] == "bob@jones.com"
-
-    customer2 = Map.put(customer, "last-name", "Smith")
-
-    assert {:ok, shown} =
-             Wasmex.Components.Instance.call_function(instance, "show-customer", [customer2])
-
-    assert {:ok, %{"customers" => customers} = state} =
-             Wasmex.Components.Instance.call_function(instance, "add-customer", [customer2, state])
-
-    assert Enum.count(customers) == 2
   end
 
   describe "error handling" do
